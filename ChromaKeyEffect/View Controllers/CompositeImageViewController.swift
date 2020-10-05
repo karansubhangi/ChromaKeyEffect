189103c1082d7567281b6cc2d006f92078c9c6a6
karansubhangi <karansubhangi@gmail.com>
2020-10-05 15:45:22 +0530

    pushing the changes

---
+++ ChromaKeyEffect/View Controllers/CompositeImageViewController.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+class CompositeImageViewController: UIViewController {
+
+    var formattedImage: CIImage?
+    let chromaKey = ChromaKey()
+    let compositedImageView = CompositedImageView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupViews()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        DispatchQueue.main.async {
+            if let image = UIImage(named: "white"),
+                let backgroundImage = CIImage(image: image),
+                let filteredImage = self.formattedImage {
+                let compositedImage = self.chromaKey.compositeImages(foregroundCIImage: filteredImage, backgroundCIImage: backgroundImage)
+                self.compositedImageView.compositedImageView.image = UIImage(ciImage: compositedImage)
+            }
+        }
+    }
+
+    init(image: CIImage) {
+        super.init(nibName: nil, bundle: nil)
+        self.formattedImage = image
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+
+    private func setupViews() {
+        setupCompositedImageView()
+    }
+
+    private func setupCompositedImageView() {
+        view.addSubview(compositedImageView)
+        compositedImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            compositedImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            compositedImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            compositedImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            compositedImageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            ])
+    }
+}
